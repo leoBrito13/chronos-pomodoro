584cc41b6614445f5346bd11b6fdc883efe6979f
Leonardo <leobrito.lnb@gmail.com>
2025-07-04 14:15:31 -0300

    adicao do formulario e do component generihtml

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,23 @@
+import { PlayCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
+import { DefaultButton } from "../DefaultButton";
+import { DefaultInput } from "../DefaultInput";
+
+export function MainForm() {
+  return (
+    <form className="form" action="">
+      <div className="formRow">
+        <DefaultInput labelText="Task" type="text" id="task" />
+      </div>
+      <div className="formRow">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+      </div>
+      <div className="formRow">
+        <Cycles />
+      </div>
+      <div className="formRow">
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
